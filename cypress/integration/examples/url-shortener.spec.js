489a3f95ba5c5b6a7ab8ed4eeb2702354cab4279
Alia Peterson <alia@pnwebs.com>
2021-03-09 10:17:46 -0700

    Create cypress files

---
+++ cypress/integration/examples/url-shortener.spec.js
@@ -0,0 +1,18 @@
+context('Url Shortener', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  describe('Home Page', () => {
+    it('Should be able to view the page title and existing shortened URLs', () => {
+
+    })
+
+    it('Should be able to view the form with the proper inputs', () => {
+
+    })
+
+    it('Should be able to fill out the form and the information should be reflected on the page', () => {
+
+    })
+  })
